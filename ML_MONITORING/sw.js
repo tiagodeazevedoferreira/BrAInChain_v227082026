23cbe5a569614f2f5b5726f23191e88ac28b33f6
tiagodeazevedoferreira <tiago.aferreira@gmail.com>
2026-08-28 11:20:55 -0300

    feat: add monitoring PWA service worker

---
+++ ML_MONITORING/sw.js
@@ -0,0 +1,1 @@
+const CACHE='brainchain-monitor-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const c=x.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return x}))));
